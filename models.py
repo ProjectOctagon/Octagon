72d358c34fc6a4ddeacc594f8791b696a71b719e
TokosEduard <jozsef.tokos05@e-uvt.ro>
2026-05-04 23:19:29 +0300

    models.py

---
+++ models.py
@@ -0,0 +1,28 @@
+from mongoengine import Document, StringField, ListField, BooleanField, IntField, ReferenceField
+
+class Person(Document):
+    meta = {'collection': 'people'}
+    PERSONNEL_NUMBER = StringField(required=True, unique=True)
+    NSN_ID = StringField()
+    Name = StringField()
+    FirstName = StringField()
+    LastName = StringField()
+    Email = StringField()
+    Country = StringField()
+    City = StringField()
+    JOB_TITLE = StringField()
+    HEADCOUNT_STATUS = StringField()
+    STATUS = StringField()
+    LineManager = StringField()  
+    TeamCode = StringField()
+
+class Organization(Document):
+    meta = {'collection': 'organizations'}
+    org_id = StringField(required=True, unique=True)
+    name = StringField()
+    parent = StringField()
+    ancestors = ListField(StringField())
+    people = ListField(StringField())  
+    children = ListField(StringField())
+    isDeleted = BooleanField(default=False)
+    sap_org_id = StringField()
